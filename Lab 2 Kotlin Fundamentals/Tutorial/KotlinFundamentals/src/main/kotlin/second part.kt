152724fa9660af34ee04d0513116d370bbc92278
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-06 09:06:08 +0300

    update

---
+++ Lab 2 Kotlin Fundamentals/Tutorial/KotlinFundamentals/src/main/kotlin/second part.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    val student = Student("Roaa" , 19)
+//    student.name = "Roaa"
+//    student.age = 19
+    println(student.name)
+    println(student.age)
+    println(student)
+}
+
+data class Student( var name : String = "" ,   var age : Int = 0)
+
+//
+//class Person{
+//    String name;
+//    int age;
+//    public Person(String name , int age){
+//        this.name = name
+//    }
+//}
+
+
+////functions
+////
+////fun display(age : Int ,  name : String){
+////    println("Your name is $name and your age is $age")
+////}
+////
+////fun add2(x:Int , y:Int):Int {
+////    return x + y
+////}
+////
+////val  add3 = fun(x:Int , y:Int) =  x + y
