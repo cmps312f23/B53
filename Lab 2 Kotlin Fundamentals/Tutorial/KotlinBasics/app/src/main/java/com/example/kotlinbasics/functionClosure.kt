adfab645f86db1c29a25823fa3e38a1aac8765d4
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-12 13:31:47 +0300

    collection functions

--- Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/functionClosure.kt
+++ Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/functionClosure.kt
@@ -2,13 +2,13 @@ package com.example.kotlinbasics
 
 //Function closure is a function that can access variables of outer function
 fun main() {
-    superFunction(isEven)
+    superFunction { number: Int -> number % 2 == 0}
     superFunction(isOdd)
 }
 
-fun superFunction(predicate: (Int) -> Boolean){
+fun superFunction(predicate: (Int) -> Boolean) {
     for (i in 1..100) {
-        if(predicate(i))
+        if (predicate(i))
             print("$i ")
     }
     println()
@@ -19,11 +19,12 @@ fun isEven(number: Int): Boolean {
 }
 
 val y = 20
+
 //lambda expression
-val x= {number : Int -> number + y }
+val x = { number: Int -> number + y }
 
-val isEven = fun (number : Int) =  number % 2 == 0
+val isEven = fun(number: Int) = number % 2 == 0
 
 
-val isOdd = fun (number : Int) =  number % 2 != 0
-fun isPositive(number : Int) =  number > 0
+val isOdd = fun(number: Int) = number % 2 != 0
+fun isPositive(number: Int) = number > 0
--- Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/lambdaExcercises.kt
+++ Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/lambdaExcercises.kt
@@ -1,7 +1,30 @@
 package com.example.kotlinbasics
 
- fun main(){
-     val numbers = listOf(1,2,3,4,5,6,7,8,9,10)
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    numbers.forEach { num: Int -> println(num) }
+    numbers.forEach { println(it) }
+
+    val sum2 = numbers.reduce { acc, next -> acc + next }
+    val sum = numbers.fold(0) { acc, next -> acc + next }
+    val doubled = numbers.map { if (it % 2 == 0) "Pass" else "Fail" }
+    val filterEven = numbers.filter { it < 0 }
+    val find9 = numbers.find { it == 19 }
+
+//     print the output for each
+    println("Sum of the list is $sum")
+    println("Sum of the list is $sum2")
+    println("Doubled list is $doubled")
+    println("Even numbers are $filterEven")
+    println("19 is $find9")
 
 
- }
+//     acc = 1 curr 2
+//     acc = 3 curr 3
+//     acc = 6 curr 4
+//     acc = 10 curr 5
+//     acc = 15 curr 6
+//     acc = 21 curr 7
+
+
+}
