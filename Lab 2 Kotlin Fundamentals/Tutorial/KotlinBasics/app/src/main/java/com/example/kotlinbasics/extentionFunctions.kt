9408859597009ff52fb5bebde2dc843cb9f214d1
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-09-12 13:07:41 +0300

    extenton fnctions

--- Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/extentionFunctions.kt
+++ Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/extentionFunctions.kt
@@ -5,27 +5,35 @@ fun main() {
     val text2 = "Hello World olol"
 
     println(text.countVowels())
+    val numbers: List<Int> = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10.square())
 
-    val numbers = listOf<Int>(1,2,3,4,5,6,7,8,9,10)
-//    println(countVowels(text2))
+
 }
 
+fun Int.square() = this * this
+
+fun mult(a: Int, b: Int): Int {
+    return a * b
+}
+
+val multip = { a: Int, b: Int -> a * b }
 
 
 fun List<Int>.filterOdd(): MutableList<Int> {
     val oddNumbers = mutableListOf<Int>()
-    for (num in this){
-        if(num % 2 !=0)
+    for (num in this) {
+        if (num % 2 != 0)
             oddNumbers.add(num)
     }
     return oddNumbers
 }
-fun String.countVowels() : Int {
+
+fun String.countVowels(): Int {
 //    val text = "Hello World"
     var count = 0
     var vowels = "aeiou"
-    for (c in this){
-        if(c in vowels)
+    for (c in this) {
+        if (c in vowels)
             count++
     }
     return count
---
+++ Lab 2 Kotlin Fundamentals/Tutorial/KotlinBasics/app/src/main/java/com/example/kotlinbasics/lambdaExcercises.kt
@@ -0,0 +1,7 @@
+package com.example.kotlinbasics
+
+ fun main(){
+     val numbers = listOf(1,2,3,4,5,6,7,8,9,10)
+
+
+ }
