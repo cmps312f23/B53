cc77488ed660b67dd82b471ec1c75ed8646f6a97
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-03 12:40:58 +0300

    new updated code

---
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/model/Stadium.kt
@@ -0,0 +1,12 @@
+package com.example.stadiums.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Stadium(
+    val name : String,
+    val city : String,
+    val status : String,
+    val imageName : String,
+    val seatingCapacity : Int,
+)
---
+++ Lab 6 - List Interaction/Tutorial/Stadiums/app/src/main/java/com/example/stadiums/repo/StadiumRepo.kt
@@ -0,0 +1,28 @@
+package com.example.stadiums.repo
+
+import android.content.Context
+import com.example.stadiums.model.Stadium
+import kotlinx.serialization.json.Json
+
+object StadiumRepo {
+    var stadiums = listOf<Stadium>()
+
+    fun getStadiums(context: Context): List<Stadium> {
+
+        if(stadiums.isEmpty()) {
+            // read the stadium.json data from the assets folder
+            val jsonTextOfTheFile = context
+                .assets
+                .open("stadiums.json")
+                .bufferedReader()
+                .use { it.readText() }
+
+            // decode them to Stadium objects
+            stadiums = Json { ignoreUnknownKeys = true }
+                .decodeFromString(jsonTextOfTheFile)
+        }
+
+        return stadiums
+
+    }
+}
