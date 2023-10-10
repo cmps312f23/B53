881b4a502076a31cfefc0b2d9cfaca9d0b4c3cb5
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-10 12:54:06 +0300

    updated code

--- Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/nav/MyNavHost.kt
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/nav/MyNavHost.kt
@@ -1,5 +1,31 @@
 package com.example.navbasics.ui.nav
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.navbasics.ui.screen.CartScreen
+import com.example.navbasics.ui.screen.FirstScreen
+import com.example.navbasics.ui.screen.Screen
+import com.example.navbasics.ui.screen.SecondScreen
 
 @Composable
+fun MyNavHost(navHostController: NavHostController) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.FirstScreen.route
+    ) {
+
+        composable(route = Screen.FirstScreen.route) {
+            FirstScreen()
+        }
+
+        composable(route = Screen.SecondScreen.route) {
+            SecondScreen()
+        }
+
+        composable(route = Screen.CartScreen.route) {
+            CartScreen()
+        }
+    }
+}
