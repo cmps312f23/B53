5a0a03400231f9b88051045b88963dacce239c1e
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-10 12:39:53 +0300

    updated code

---
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/Screen.kt
@@ -0,0 +1,7 @@
+package com.example.navbasics.ui.screen
+
+import android.graphics.drawable.Icon
+
+sealed class Screen(val route: String, val title: String, val icon: Icon) {
+//    declare objects
+}
--- Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/SecondScreen.kt
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/SecondScreen.kt
@@ -12,6 +12,18 @@ import androidx.compose.ui.unit.sp
 import com.example.navbasics.ui.theme.NavBasicsTheme
 
 
+/*
+    1. We need to create a class [sealed] Screen that helps us map , some constant values
+     - route , Icon and the title
+    2. Navigation Host [Graph] Mapping
+        --Route to Composable
+        --It can extract the parameters in the Path [Route]
+        --localhost:3000/api/students/1 ?query =hello
+    3. Scafold [bottomBar -NavigationBar , topBar - TopAppBar ] ,
+        content part of the scafold will be NavigationHost created in step 2
+    4. navigationController.navigate(PATH)  -PATH = "cart" , "cart/1" "cart/1?name=banana
+ */
+
 @Composable
 fun SecondScreen(modifier: Modifier = Modifier) {
     Box(
