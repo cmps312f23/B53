51b9a3e038c181c9cc0ec88e7c43e5dcb369f9c1
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-10 12:44:34 +0300

    updated code

---
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/nav/MyNavHost.kt
@@ -0,0 +1,5 @@
+package com.example.navbasics.ui.nav
+
+import androidx.compose.runtime.Composable
+
+@Composable
--- Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/Screen.kt
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/Screen.kt
@@ -1,7 +1,15 @@
 package com.example.navbasics.ui.screen
 
-import android.graphics.drawable.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ScreenShare
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class Screen(val route: String, val title: String, val icon: Icon) {
-//    declare objects
+//constants you do not want to hard code, this class is not mandatory
+//it just helps you not to make mistakes while typing the route or title etc...
+sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
+    //    declare objects
+    object CartScreen : Screen("cart", "Cart", Icons.Default.ShoppingCart)
+    object FirstScreen : Screen("first-screen", "First Screen", Icons.Default.ScreenShare)
+    object SecondScreen : Screen("second-screen", "Second Screen", Icons.Default.ScreenShare)
 }
