c4d947cee2fe891321b4e0f00605b24917e3d2a0
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-10 12:22:43 +0300

    updated code

---
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/FirstScreen.kt
@@ -0,0 +1,20 @@
+package com.example.navbasics.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navbasics.ui.theme.NavBasicsTheme
+
+
+@Composable
+fun FirstScreen(modifier: Modifier = Modifier){
+
+}
+
+@Preview
+@Composable
+fun FirstScreenPreview(){
+    NavBasicsTheme {
+        FirstScreen()
+    }
+}
