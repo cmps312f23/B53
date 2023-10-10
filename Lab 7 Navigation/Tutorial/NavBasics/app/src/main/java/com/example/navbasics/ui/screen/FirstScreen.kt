02ab4c1e2c3134af97dba10988e06e40767cb353
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-10 12:28:27 +0300

    updated code

--- Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/FirstScreen.kt
+++ Lab 7 Navigation/Tutorial/NavBasics/app/src/main/java/com/example/navbasics/ui/screen/FirstScreen.kt
@@ -1,19 +1,34 @@
 package com.example.navbasics.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.navbasics.ui.theme.NavBasicsTheme
 
 
 @Composable
-fun FirstScreen(modifier: Modifier = Modifier){
-
+fun FirstScreen(modifier: Modifier = Modifier) {
+    Box(
+        Modifier.background(MaterialTheme.colorScheme.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "First Screen",
+            color = MaterialTheme.colorScheme.onPrimary,
+            fontSize = 25.sp
+        )
+    }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
-fun FirstScreenPreview(){
+fun FirstScreenPreview() {
     NavBasicsTheme {
         FirstScreen()
     }
