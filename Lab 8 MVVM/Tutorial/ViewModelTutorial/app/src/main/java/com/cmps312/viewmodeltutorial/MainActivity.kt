d96094ed348d4eb68914695a172599d3e677d533
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-10-31 12:34:28 +0300

    updated

--- Lab 8 MVVM/Tutorial/ViewModelTutorial/app/src/main/java/com/cmps312/viewmodeltutorial/MainActivity.kt
+++ Lab 8 MVVM/Tutorial/ViewModelTutorial/app/src/main/java/com/cmps312/viewmodeltutorial/MainActivity.kt
@@ -31,10 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Students()
 }
 
 @Preview(showBackground = true)
--- Lab 8 MVVM/Tutorial/ViewModelTutorial/app/src/main/java/com/cmps312/viewmodeltutorial/Students.kt
+++ Lab 8 MVVM/Tutorial/ViewModelTutorial/app/src/main/java/com/cmps312/viewmodeltutorial/Students.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 data class Student(val name : String , val age : Int)
 
