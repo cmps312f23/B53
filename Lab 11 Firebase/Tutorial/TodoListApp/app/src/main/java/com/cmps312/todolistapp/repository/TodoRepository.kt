704a44b7d6f8bd28b5e4c1b4daf4b34c76b84ac4
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-11-21 13:05:42 +0300

    updated

--- Lab 11 Firebase/Tutorial/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
+++ Lab 11 Firebase/Tutorial/TodoListApp/app/src/main/java/com/cmps312/todolistapp/repository/TodoRepository.kt
@@ -1,33 +1,45 @@
 package com.cmps312.todolistapp.repository
 
-import android.util.Log
 import com.cmps312.todolistapp.model.Project
-import com.cmps312.todolistapp.model.Todo
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.tasks.await
 
 class TodoRepository {
 
+    //    declare the database
+    private val db: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
 
-//    fun observeProjects(): Flow<List<Project>>
-//
-//    fun addProject(project: Project)
-//
-//    fun updateProject(project: Project)
-//
-//    suspend fun deleteProject(project: Project)
-//
+    //    Get a reference to the projects and todos collections to not write them again and again
+    private val projectsRef = db.collection("projects")
+    private val todosRef = db.collection("todos")
+
+
+    fun addProject(project: Project) = projectsRef.add(project)
+    fun updateProject(project: Project) = projectsRef
+        .document(project.id)
+        .set(project)
+
+    fun deleteProject(project: Project) = projectsRef
+        .document(project.id)
+        .delete()
+
+    fun observeProjects(): Flow<List<Project>> = callbackFlow {
+        val snapshotListener = projectsRef.addSnapshotListener { values, err ->
+            if (err != null)
+                return@addSnapshotListener
+
+            val projects = values!!.toObjects(Project::class.java)
+            trySend(projects)
+        }
+        awaitClose { snapshotListener.remove() }
+    }
+
 //    fun observeTodos(pid: String): Flow<List<Todo>>
-//
 //    suspend fun getTodo(id: String):
 //    suspend fun addTodo(todo: Todo)
-//
-//
 //    fun updateTodo(todo: Todo)
 //    fun deleteTodo(todo: Todo)
-//
 //    suspend fun getTodoListByProject(pid: String)
 }
